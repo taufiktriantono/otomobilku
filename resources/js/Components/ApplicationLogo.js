403a64f4ca138eb03340e679cc0388481208d69f
Taufik Triantono <taufik.triantono@ottodigital.id>
2022-06-17 13:25:02 +0700

    fix - application logo uri

--- resources/js/Components/ApplicationLogo.js
+++ resources/js/Components/ApplicationLogo.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function ApplicationLogo({ className }) {
     return (
-        <img className={className} src={'http://otomobilku.test/storage/images/otomobilku_fix.png'} />
+        <img className={className} src={'http://otomobilku.id/storage/images/otomobilku_fix.png'} />
     );
 }
