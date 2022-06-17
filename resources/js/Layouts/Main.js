b68551fd96875f921bb63c446a43bd71e0a7309e
Taufik Triantono <taufik.triantono@ottodigital.id>
2022-06-17 15:11:31 +0700

    fix

--- resources/js/Layouts/Main.js
+++ resources/js/Layouts/Main.js
@@ -65,7 +65,7 @@ export default function Layout(props) {
                 <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
                   <div className="flex justify-start lg:w-0 lg:flex-1">
                       <Link href="/">
-                        <ApplicationLogo className="w-auto sm:h-20 h-20" src='http://otomobilku.id/storage/images/otomobilku_fix.png'/>
+                        <ApplicationLogo className="w-auto sm:h-10 h-8" src='http://otomobilku.id/storage/images/otomobilku_fix.png'/>
                       </Link>
                       <div className="group relative mt-1">
                           <form onSubmit={submit} className="flex justify-center ml-10 border rounded-md">
